634955573b06f8ba3319d1c669561d5d856a9990
NattyZelB <92927774+NattyZelB@users.noreply.github.com>
2023-06-26 16:53:29 +0200

    modified decouple

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'foodonline_main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
 
