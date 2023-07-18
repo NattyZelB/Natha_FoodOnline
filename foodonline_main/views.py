43fd70d2e6bb8ff856f58efac0eadc4c44f9dccc
NattyZelB <92927774+NattyZelB@users.noreply.github.com>
2023-07-18 12:45:34 +0200

    add, decrease, delete and calulate price in cart

--- foodonline_main/views.py
+++ foodonline_main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from vendor.models import Vendor
 def home(request):
-    return render(request, 'home.html')
+    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+    context = {
+        'vendors': vendors,
+    }
+    print(vendors)
+    return render(request, 'home.html', context)
---
+++ marketplace/context_processors.py
@@ -0,0 +1,32 @@
+import decimal
+
+from .models import Cart
+from menu.models import FoodItem
+from decimal import Decimal
+
+def get_cart_counter(request):
+    cart_count = 0
+    if request.user.is_authenticated:
+        try:
+            cart_items = Cart.objects.filter(user=request.user)
+            if cart_items:
+                for cart_item in cart_items:
+                    cart_count += cart_item.quantity
+            else:
+                cart_count = 0
+        except:
+            cart_count = 0
+    return dict(cart_count=cart_count)
+
+def get_cart_amounts(request):
+    subtotal = 0
+    tax = 0
+    grand_total = 0
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+        for item in cart_items:
+            fooditem = FoodItem.objects.get(pk=item.fooditem.id)
+            subtotal += (fooditem.price * item.quantity)
+        tax = round(Decimal((subtotal * 21)/100), 2)
+        grand_total = round(Decimal(subtotal + tax), 2)
+    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total)
